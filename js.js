f72f03a9b9dc64913f00d517dabbbc3c3ea9817e
Scott <scottpmccann@gmail.com>
2022-04-16 17:03:00 -0700

    refactor css, html and clean-up js

--- js.js
+++ js.js
@@ -1,23 +1,10 @@
-const add = (x,y) => {
-    return x + y;
-}
+const add = (x,y) =>  x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => (y === 0) ? "divide by zero" : x / y;
 
-const subtract = (x, y) => {
-    return x - y;
-}
-
-const multiply = (x, y) => {
-    return x * y;
-}
-
-const divide = (x, y) => {
-    if (y === 0) {
-        return "divide by zero"
-    } else {
-        return x / y;
-    }
-}
-
+const buttons = document.querySelectorAll('.numpad')
+const screen = document.querySelector('.screen')  
 
 const operate = (operator, x, y) => {
     if (operator === "+") {
@@ -38,46 +25,45 @@ const display = () => {
     let num1 = "";
     let num2 = "";
     let op = "";
-    const buttons = document.querySelectorAll('.numpad')
-    const screen = document.querySelector('.screen')  
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            let anOperator = ops.includes(button.textContent);
-            let aNumber = nums.includes(button.textContent);
-            if (op === "" && anOperator || op !== "" && num2 === "" && anOperator){
-                op = button.textContent;
+            const userInputs = button.textContent;
+            const anOperator = ops.includes(userInputs); 
+            const aNumber = nums.includes(userInputs);  
+            if (op === "" && anOperator && num1 !== "" || op !== "" && num2 === "" && anOperator){ 
+                op = userInputs;
                 screen.textContent =  `${num1} ${op} ${num2}`;
-            } else if (op !== "" && aNumber) {
-                num2 += button.textContent;
+            } else if (op !== "" && aNumber) {  
+                num2 += userInputs;
                 screen.textContent =  `${num1} ${op} ${num2}`;
-            } else if (aNumber) {
-                num1 += button.textContent;
+            } else if (aNumber) {      
+                num1 += userInputs;
                 screen.textContent =  `${num1} ${op} ${num2}`;
             } else {
                 let input = [op,  num1, num2]
-                let calculate = operate(input[0],parseFloat(input[1]), parseFloat(input[2]))
+                let calculate = operate(input[0],parseFloat(input[1]), parseFloat(input[2]))  
                 console.log(input)
-                if (button.textContent === "=") {
-                    if (!input.includes("")) {
+                if (userInputs === "=") {  
+                    if (!input.includes("")) {      
                         screen.textContent = calculate
-                        num1 = screen.textContent;
+                        num1 = calculate;
                         num2 = "";
                         op = ""; 
                     } else {
                         screen.textContent = num1
                     }        
-                } else if (op.length === 1 && anOperator) {
+                } else if (op.length === 1 && anOperator) {  
                     num1 = calculate;
                     num2 = ""; 
-                    op = button.textContent;
+                    op = userInputs;
                     screen.textContent =  `${num1} ${op} ${num2}`;
-                } else if (button.textContent === "Clear") {
+                } else if (userInputs === "Clear All") {  
                     screen.textContent = 0;
                     num1 = "";
                     num2 = "";
                     op = "";
                     input = "";
-                } else if (button.textContent === "(-)") {
+                } else if (userInputs === "-/+") { 
                     if (num2 === ""){
                         num1 *= -1;
                         screen.textContent =  `${num1} ${op} ${num2}`
@@ -88,8 +74,6 @@ const display = () => {
                 }
             }
         });
-    });
-    
+    }); 
 }
-
 display()
